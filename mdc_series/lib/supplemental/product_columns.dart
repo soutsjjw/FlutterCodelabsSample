bff37d48911512ad7b3085eaf7569cd74f6f3001
soutsjjw <souts.jjw@gmail.com>
2021-02-02 14:37:35 +0800

    MDC Flutter 教程 4：Material 组件进阶

--- mdc_series/lib/supplemental/product_columns.dart
+++ mdc_series/lib/supplemental/product_columns.dart
@@ -34,23 +34,24 @@ class TwoProductCardColumn extends StatelessWidget {
       double heightOfCards = (constraints.biggest.height - spacerHeight) / 2.0;
       double heightOfImages = heightOfCards - ProductCard.kTextBoxHeight;
       // TODO: Change imageAspectRatio calculation (104)
-      double imageAspectRatio = constraints.biggest.width / heightOfImages;
+      double imageAspectRatio =
+      (heightOfImages >= 0.0 && constraints.biggest.width > heightOfImages)
+          ? constraints.biggest.width / heightOfImages
+          : 33 / 49;
 
       // TODO: Replace Column with a ListView (104)
-      return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+      return ListView(
         children: <Widget>[
           Padding(
             padding: EdgeInsetsDirectional.only(start: 28.0),
             child: top != null
                 ? ProductCard(
-                    imageAspectRatio: imageAspectRatio,
-                    product: top,
-                  )
+              imageAspectRatio: imageAspectRatio,
+              product: top,
+            )
                 : SizedBox(
-                    height: heightOfCards,
-                  ),
+              height: heightOfCards,
+            ),
           ),
           SizedBox(height: spacerHeight),
           Padding(
@@ -74,14 +75,14 @@ class OneProductCardColumn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: Replace Column with a ListView (104)
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
+    return ListView(
+      reverse: true,
       children: <Widget>[
-        ProductCard(
-          product: product,
-        ),
         SizedBox(
           height: 40.0,
+        ),
+        ProductCard(
+          product: product,
         ),
       ],
     );
